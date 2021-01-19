356f348f642f1fffb0aae30fa3d98bb87d153eb4
Bruno <bruno.umbelino0@gmail.com>
2021-01-18 22:44:33 -0300

    improving the style and starting the development of the calculation algorithms

--- src/components/ButtonsComponent.js
+++ src/components/ButtonsComponent.js
@@ -1,29 +1,42 @@
 import React from "react";
 import { Button } from "reactstrap";
 
-const values = [
-  "AC",
-  "/",
-  "7",
-  "8",
-  "9",
-  "x",
-  "4",
-  "5",
-  "6",
-  "-",
-  "1",
-  "2",
-  "3",
-  "+",
-  "0",
-  ".",
-  "=",
+const BtnValues = [
+  { id: "clear", simb: "AC" },
+  {id: 'backspace', simb: 'DEL'},
+  { id: "divide", simb: "/" },
+  { id: "seven", simb: "7" },
+  { id: "eight", simb: "8" },
+  { id: "nine", simb: "9" },
+  { id: "multiply", simb: "x" },
+  { id: "four", simb: "4" },
+  { id: "five", simb: "5" },
+  { id: "six", simb: "6" },
+  { id: "subtract", simb: "-" },
+  { id: "one", simb: "1" },
+  { id: "two", simb: "2" },
+  { id: "three", simb: "3" },
+  { id: "add", simb: "+" },
+  { id: "zero", simb: "0" },
+  { id: "decimal", simb: "." },
+  { id: "equals", simb: "=" },
 ];
 
-const Btns = () => {
-  const Btns = values.map((item) => {
-    return <Button className={item}>{item}</Button>;
+const Btns = (props) => {
+  const Btns = BtnValues.map((item) => {
+    var clickBehavior = (id) => {
+      props.handleInput(id);
+    };
+    if (item.id === "clear") {
+      clickBehavior = props.clearDisplay;
+    } else if (item.id === "equals") {
+      clickBehavior = props.calculate;
+    }
+    return (
+      <Button key={item.id} id={item.id} onClick={clickBehavior}>
+        {item.simb}
+      </Button>
+    );
   });
   return <div className="btns">{Btns}</div>;
 };
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,14 +3,71 @@ import Btns from "./ButtonsComponent";
 import { Container, Row } from "reactstrap";
 
 export class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { result: "10+10+10" };
+    this.clearDisplay = this.clearDisplay.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.calculate = this.calculate.bind(this);
+  }
+
+  clearDisplay() {
+    this.setState({ result: 0 });
+  }
+
+  handleInput(ev) {
+    console.log(ev.target.innerText);
+    let value = ev.target.innerText;
+    this.setState({ result: this.state.result + value });
+  }
+
+  calculate() {
+    let values = this.state.result;
+    let numbers = values.match(/(\d+)/g).map((num) => parseInt(num));
+    let arithExp = values.match(/(\W)/g);
+    var x = 0;
+
+    if (numbers.length == 1) return;
+
+    for (let i = numbers.length - 1; i >= 0; i--) {
+      console.log(i, numbers[i], numbers[i - 1]);
+
+      let number1 = numbers[i];
+      let number2 = numbers[i--];
+
+      if (number2 === undefined) {
+        number2 = 0;
+      }
+
+      switch (arithExp[i]) {
+        case "+":
+          x = x + (number1 + number2);
+          console.log("soma: ", number1, number2);
+          console.log("parcial: " + x);
+      }
+    }
+    console.log("result: " + x);
+    console.log(numbers, arithExp);
+  }
+
   render() {
     return (
       <div className="main">
         <div className="calculator-wrap">
           <div className="calculator">
             <Container>
-              <Row>dysplay</Row>
-              <Row> <Btns /> </Row>
+              <Row>
+                <div>Display:</div>
+                <div id="display">{this.state.result}</div>
+              </Row>
+              <Row>
+                {" "}
+                <Btns
+                  clearDisplay={this.clearDisplay}
+                  handleInput={this.handleInput}
+                  calculate={this.calculate}
+                />{" "}
+              </Row>
             </Container>
           </div>
         </div>
